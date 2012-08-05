61942752f304e39544498b7ca6fb0966d673c18d
LUbuntu <lubuntu@lubuntu-laptop.(none)>
2012-08-05 19:44:46 +0700

    host jadi ryan.pakar

--- psutil_partition.py
+++ psutil_partition.py
@@ -18,7 +18,7 @@ for partition in partitions :
 	c = pycurl.Curl()
 	data = [('IP',IP),('device',dev),('filetype',filestype),('mount_on',mount)]
 	resp_data = urllib.urlencode(data)
-	c.setopt(pycurl.URL, 'http://rully.tr4c3r.dev/HDD-Monitoring/hdd_monitor/index.php/site/add_disk_partition')
+	c.setopt(pycurl.URL, 'http://ryan.pakar/HDD-Monitoring/hdd_monitor/index.php/site/add_disk_partition')
 	c.setopt(pycurl.POST, 1)
 	c.setopt(pycurl.POSTFIELDS, resp_data)
 	c.perform()
